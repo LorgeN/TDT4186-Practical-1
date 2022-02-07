ac1025941cf6093ea9cdf1bcc0bdc3aa330def9c
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-02-07 14:41:26 +0100

    Add includes and correct pointer type

--- src/alarms.c
+++ src/alarms.c
@@ -1,12 +1,13 @@
 #include "alarms.h"
 #include <signal.h>
+#include <string.h>
 
 unsigned int alarm_count = 0;
 alarm_t alarms[MAX_ALARMS];
 
 alarm_t get_alarm(unsigned int id)
 {
-    return alarms[id]; 
+    return alarms[id];
 }
 
 unsigned int get_alarm_count()
@@ -24,7 +25,7 @@ unsigned int schedule_alarm(time_t timestamp)
     unsigned int alarm_id = alarm_count;
     alarms[alarm_id] = new_alarm;
     alarm_count += 1;
-    
+
     return alarm_id;
 }
 
@@ -33,7 +34,7 @@ void cancel_alarm(unsigned int id)
     alarm_t alarm = get_alarm(id);
     kill(alarm.pid, SIGKILL);
 
-    alarm_t* dest = &alarms + id;
+    alarm_t *dest = alarms + id;
     memmove(dest, dest + 1, sizeof(alarm_t) * (alarm_count - id - 1));
     memset(&alarms + alarm_count - 1, 0, sizeof(alarm_t));
     alarm_count -= 1;
