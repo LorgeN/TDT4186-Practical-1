eb0f35fb4dd54bb4e5b290a8dc3e0798a3590ff3
Eirik Lorgen Tanberg <eirik@tanberg.org>
2022-02-07 14:38:08 +0100

    Basic alarm control

--- src/alarms.c
+++ src/alarms.c
@@ -1,4 +1,5 @@
 #include "alarms.h"
+#include <signal.h>
 
 unsigned int alarm_count = 0;
 alarm_t alarms[MAX_ALARMS];
@@ -15,10 +16,25 @@ unsigned int get_alarm_count()
 
 unsigned int schedule_alarm(time_t timestamp)
 {
-    // TODO
+    alarm_t new_alarm;
+    new_alarm.timestamp = timestamp;
+
+    // TODO: Schedule alarm and set PID
+
+    unsigned int alarm_id = alarm_count;
+    alarms[alarm_id] = new_alarm;
+    alarm_count += 1;
+    
+    return alarm_id;
 }
 
 void cancel_alarm(unsigned int id)
 {
-    // TODO
+    alarm_t alarm = get_alarm(id);
+    kill(alarm.pid, SIGKILL);
+
+    alarm_t* dest = &alarms + id;
+    memmove(dest, dest + 1, sizeof(alarm_t) * (alarm_count - id - 1));
+    memset(&alarms + alarm_count - 1, 0, sizeof(alarm_t));
+    alarm_count -= 1;
 }
