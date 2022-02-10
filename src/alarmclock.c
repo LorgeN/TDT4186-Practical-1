041065c61105400b8e71e5fed88b13dafd6b3669
erik98m <erik98m@hotmail.com>
2022-02-10 16:34:46 +0100

    my message

--- src/alarmclock.c
+++ src/alarmclock.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "alarms.h"
 
+int menu() {
+    return 0;
+}
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello world!\n");
+    printf("Extended version with NFTS comming soon!\n");
 
     time_t rawtime;
     struct tm *timeinfo;
@@ -11,6 +15,13 @@ int main(int argc, char const *argv[])
     time(&rawtime);
     timeinfo = localtime(&rawtime);
     printf("Current local time and date: %s\n", asctime(timeinfo));
+
+    char choice;
+    printf("Please enter \"s\" for schedule a new alarm\n");
+    printf("Please enter \"l\" for see list of active alarms\n");
+    printf("Please enter \"c\" for canceling a scheduled alarm\n");
+    printf("Please enter \"x\" for exit the program\n");
+    scanf("%s", &choice);
 
     unsigned int id = schedule_alarm(rawtime);
     printf("%d \n", id);
